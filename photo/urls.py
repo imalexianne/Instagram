ab8565f336d58bc9ef45e24098a41a478ce03c98
imalexianne <imanirax@yahoo.fr>
2019-03-26 14:22:42 +0200

    add changes

--- photo/urls.py
+++ photo/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('images.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
+    
     url(r'^tinymce/', include('tinymce.urls')), 
 ]
